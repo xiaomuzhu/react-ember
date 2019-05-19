becd397f4cdc81d3551a1e2598db10a64b0e868d
xiaomuzhu <meituandxy@gmail.com>
2019-05-19 14:47:58 +0800

    feat(examples): add count demo

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,10 @@
 import { MiddlewareAPI, Dispatch, AnyAction } from 'redux'
 
 export let dispatch: Dispatch<AnyAction>
-let getState: any
 
 export function createMiddleware() {
   return (middlewareAPI: MiddlewareAPI) => {
     dispatch = middlewareAPI.dispatch
-    getState = middlewareAPI.getState
 
     return (next: Dispatch<AnyAction>) => (action: any) => {
       let result = next(action)
